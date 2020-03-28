130b65e7c57a67e2c66c60f9240e697591fb3270
gfixler <gary@garyfixler.com>
2020-03-27 17:58:53 -0700

    Replace get/set methods with passed-in functions
    I didn't like the weirdness of get/set having to take the first, curried
    arg, then call the stored _get/_set values with that arg, as it didn't
    match up between get, which took the structure, and set, which took the
    value to set, and returned a partially applied function, which would
    take the structure. This was hard to see in the code, and I realized I
    just don't need to forward things through those methods. I just need to
    store get/set as properties. Well, I hope that's all I need...

--- pylens/lens.py
+++ pylens/lens.py
@@ -7,24 +7,17 @@ class Lens (object):
     """
 
     def __init__ (self, getter, setter):
-        self._get = getter
-        self._set = setter
-
-    def get (self, structure):
-        return self._get(structure)
-
-    def set (self, value):
-        return self._set(value)
+        self.get = getter
+        self.set = setter
 
     def modify (self, f):
-        return lambda s: self._set(f(self._get(s)))(s)
+        return lambda s: self.set(f(self.get(s)))(s)
 
     def __or__ (self, other):
         """
         Compose Lenses with the pipe char.
         """
-        composedGet = comp2(other._get)(self._get)
-        # from paper: put y s = put1 (put2 y (get1 s)) s
-        composedSet = lambda v: lambda s: self._set(other._set(v)(self._get(s)))(s)
+        composedGet = comp2(other.get)(self.get)
+        composedSet = lambda v: lambda s: self.set(other.set(v)(self.get(s)))(s)
         return Lens(composedGet, composedSet)
 
