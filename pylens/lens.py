bd66bc6fd04732de4215cb85b2ecfc5e55396cb5
gfixler <gary@garyfixler.com>
2020-03-28 04:57:16 -0700

    Add ImpureLens
    I don't know if this is a great idea, but I ran into two issues trying
    to use this library in Autodesk Maya, to create a lens to target the xyx
    translations of transforms. The first was that, because I was simply
    calling impure "actions" on objects in the scene, and not manipulating
    values passed into a function, which I could pull apart to manipulate
    some internal element, and then reassemble into a copy, immutably, I was
    forgetting to return the new value. The second was that, when I
    remembered to return the value, which ballooned the single call to the
    command in Maya, I further realized that I could lock that value in
    Maya, which would cause set and modify to fail silently without changing
    it, and my cumbersome functions were just returning the value passed in,
    as though everything had worked out, which is a bad assumption to make
    when sending orders into the world outside the program. ImpureLens gets
    around this by simply deriving (by subclassing) everything from Lens,
    but overriding the init to wrap the passed-in setter, such that it also
    calls the passed-in getter again, after the set operation, and returns
    that new (or still old, if things failed) value.

--- pylens/lens.py
+++ pylens/lens.py
@@ -40,3 +40,19 @@ class Lens (object):
         composedSet = lambda v: lambda s: self.set(other.set(v)(self.get(s)))(s)
         return Lens(composedGet, composedSet)
 
+
+class ImpureLens (Lens):
+    """
+    Impure lenses are meant for values that don't pull apart and go back
+    together like the values in typical, pure lenses. An example would be a
+    setting in a tool or program that's locked, and holds its value through
+    attempts to set it. For these values, the only reliable way to return the
+    new value is to get it again, after setting it. That's what this class
+    does. It derives from Lens, and simply overrides the init method to wrap
+    the setter passed in, such that after it sets, it calls the passed-in
+    getter again, and returns the value it receives.
+    """
+    def __init__ (self, getter, setter):
+        self.get = getter
+        self.set = lambda v: lambda x: [setter(v)(x), getter(x)][1]
+
