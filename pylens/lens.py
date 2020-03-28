c184276307690621f4963f285331d824d1766b74
gfixler <gary@garyfixler.com>
2020-03-27 18:03:06 -0700

    Add some docstring documentation to Lens

--- pylens/lens.py
+++ pylens/lens.py
@@ -7,6 +7,21 @@ class Lens (object):
     """
 
     def __init__ (self, getter, setter):
+        """
+        getter and setter each target the same element of a structure
+
+        getter is a function that takes the structure and returns the element
+
+        setter is a function that takes a value, and returns a function which
+        takes the structure, and returns a copy, with the element replaced by
+        the value
+
+        Example Lens onto the first element in a 2-tuple:
+
+        first_get = lambda (x, _): x
+        first_set = lambda v: lambda (x, y): (v, y)
+        first = Lens(first_get, first_set)
+        """
         self.get = getter
         self.set = setter
 
@@ -16,6 +31,10 @@ class Lens (object):
     def __or__ (self, other):
         """
         Compose Lenses with the pipe char.
+
+        Example:
+
+        compositeLens = lensA | lensB | lensC
         """
         composedGet = comp2(other.get)(self.get)
         composedSet = lambda v: lambda s: self.set(other.set(v)(self.get(s)))(s)
