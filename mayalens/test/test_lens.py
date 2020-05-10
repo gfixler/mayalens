2e1efb5e3221fa61f3180f8fe3c48d684793ff57
gfixler <gary@garyfixler.com>
2020-05-10 01:41:04 -0700

    Begin testing Lens
    I decided to test the most simple case of a targetable sub-structure,
    the entire structure itself. This revealed that the Lens' get and set
    functions were now just id and const. Neat.

---
+++ mayalens/test/test_lens.py
@@ -0,0 +1,21 @@
+import unittest
+from nose.plugins.attrib import attr
+
+from hypothesis import given
+import hypothesis.strategies as st
+
+from ..lens import Lens, ImpureLens
+
+
+ident = lambda x: x
+const = lambda x: lambda _: x
+
+
+class Test_Lens (unittest.TestCase):
+
+    def test_canCreateIdentityLens (self):
+        myLens = Lens(ident, const)
+        self.assertEquals(myLens.get("foo"), "foo")
+        self.assertEquals(myLens.set("bar")("foo"), "bar")
+        self.assertEquals(myLens.modify(const("baz"))("foo"), "baz")
+
